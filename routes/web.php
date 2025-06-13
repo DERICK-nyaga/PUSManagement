d06d7f06ae648f7cf11154d6a54fbf32d125738e
DERICK-nyaga <dericknyagam@gmail.com>
2025-06-13 08:58:19 +0300

    Removed some route

--- routes/web.php
+++ routes/web.php
@@ -4,15 +4,17 @@ use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\DashboardController;
 use App\Http\Controllers\StationController;
 use App\Http\Controllers\EmployeeController;
+use App\Http\Controllers\LossController;
 
 Route::get('/', [DashboardController::class, 'index']);
 Route::get('/dashboard', function () {
     return view('dashboard');
 })->name('dashboard');
-// Route::get('/stations', function () {
-//     return view('stations');
-// })->name('stations');
 
 
 Route::resource('/stations', StationController::class);
 Route::resource('/employees', EmployeeController::class);
+Route::resource('/losses', LossController::class);
+Route::get('stations/{station}/losses', [LossController::class, 'stationLosses'])->name('stations.losses');
+Route::get('/employees/{employee}/edit', [EmployeeController::class, 'edit'])->name('employees.edit');
+Route::put('/employees/{employee}', [EmployeeController::class, 'update'])->name('employees.update');
