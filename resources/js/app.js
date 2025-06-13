6f348d0c401dbe71b4a675535a88d13fa82572c2
DERICK-nyaga <dericknyagam@gmail.com>
2025-06-13 13:31:21 +0300

    modified javascript files

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,1 +1,25 @@
 import './bootstrap';
+document.addEventListener('DOMContentLoaded', function() {
+    // Mobile menu toggle
+    const sidebar = document.querySelector('.sidebar');
+    const menuToggler = document.querySelector('.navbar-toggler');
+
+    if (menuToggler) {
+        menuToggler.addEventListener('click', function() {
+            sidebar.classList.toggle('show');
+        });
+    }
+
+    // Submenu toggle
+    const hasSubmenu = document.querySelectorAll('.has-submenu > .nav-link');
+
+    hasSubmenu.forEach(item => {
+        item.addEventListener('click', function(e) {
+            if (window.innerWidth < 992) {
+                e.preventDefault();
+                const parent = this.parentElement;
+                parent.classList.toggle('show');
+            }
+        });
+    });
+});
