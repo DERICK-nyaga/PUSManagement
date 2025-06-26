9053e78fd08dd7b8c3141b7ad191ecbfdf4fb142
unknown <dericknyagam@gmail.com>
2025-06-26 16:01:49 +0300

    modified user

--- app/Models/User.php
+++ app/Models/User.php
@@ -6,16 +6,18 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
+use Illuminate\Database\Eloquent\SoftDeletes;
 
 class User extends Authenticatable
 {
-
+    use Notifiable, SoftDeletes;
     use HasFactory, Notifiable;
 
     protected $fillable = [
         'name',
         'email',
         'password',
+        'role'
     ];
 
     protected $hidden = [
@@ -29,5 +31,18 @@ class User extends Authenticatable
             'email_verified_at' => 'datetime',
             'password' => 'hashed',
         ];
+    }
+    public function createdPayments()
+    {
+        return $this->hasMany(Payment::class, 'created_by');
+    }
+
+    public function approvedPayments()
+    {
+        return $this->hasMany(Payment::class, 'approved_by');
+    }
+    public function creator()
+    {
+        return $this->belongsTo(User::class, 'created_by');
     }
 }
