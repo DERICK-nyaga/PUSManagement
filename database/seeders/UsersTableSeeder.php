7765697936287db721c733281e8f2364e045dc34
unknown <dericknyagam@gmail.com>
2025-06-26 13:58:24 +0300

    seeder for users to simulate real users

---
+++ database/seeders/UsersTableSeeder.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\Hash;
+
+class UsersTableSeeder extends Seeder
+{
+    public function run()
+    {
+        \App\Models\User::create([
+            'name' => 'Staff User',
+            'email' => 'staff@example.com',
+            'password' => Hash::make('password'),
+            'role' => 'staff'
+        ]);
+    }
+}
