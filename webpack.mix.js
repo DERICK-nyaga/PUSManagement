7e95004161144016f61e0224df15ce2b0553903d
unknown <dericknyagam@gmail.com>
2025-06-26 14:25:41 +0300

    webpack mod

--- webpack.mix.js
+++ webpack.mix.js
@@ -5,6 +5,10 @@ mix.postCss('/css/modifiedstyles.css', 'public/css', [
     require('postcss-import'),
     require('autoprefixer'),
 ]);
+mix.postCss('/css/fixedstyles.css', 'public/css', [
+    require('postcss-import'),
+    require('autoprefixer'),
+]);
 // const mix = require('laravel-mix');
 
 mix.js('resources/js/app.js', 'public/js')
@@ -12,5 +16,12 @@ mix.js('resources/js/app.js', 'public/js')
        require('postcss-import'),
        require('autoprefixer'),
    ]);
-// Add versioning for cache busting (optional)
-// mix.version();
+
+   mix.js('resources/js/app.js', 'public/js')
+   .postCss('resources/css/fixedstyles.css', 'public/css', [
+       require('postcss-import'),
+       require('autoprefixer'),
+   ]);
+
+mix.js('resources/js/payments.js', 'public/js')
+   .postCss('resources/css/modifiedstyles.css', 'public/css');
