be88fd221f98d63e710c2da60d9b7dfea208478e
unknown <dericknyagam@gmail.com>
2025-06-26 15:57:53 +0300

    stations index page

--- resources/views/stations/index.blade.php
+++ resources/views/stations/index.blade.php
@@ -1,6 +1,7 @@
 @extends('layouts.app')
 
 @section('content')
+
 <div class="container">
     <div class="card shadow-sm">
         <div class="card-header d-flex justify-content-between align-items-center">
