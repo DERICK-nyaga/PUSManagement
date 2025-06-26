fdead1cce82554c74c2d2bf6cc67d776a81d36d1
unknown <dericknyagam@gmail.com>
2025-06-26 16:13:47 +0300

    config/filesystems.php 2.0

--- config/filesystems.php
+++ config/filesystems.php
@@ -37,6 +37,12 @@ return [
             'throw' => false,
             'report' => false,
         ],
+        
+        'reports' => [
+            'driver' => 'local',
+            'root' => storage_path('app/reports'),
+            'visibility' => 'private',
+        ],
 
         'public' => [
             'driver' => 'local',
